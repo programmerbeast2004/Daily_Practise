e327106e2cb75b57546c3c73dee9791385082c3d
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-07-31 12:19:36 +0530

    Create 805-EscapeTheGhosts

---
+++ 805-EscapeTheGhosts/805-EscapeTheGhosts.java
@@ -0,0 +1,11 @@
+// Last updated: 7/31/2025, 12:19:38 PM
+class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int max = Math.abs(target[0]) + Math.abs(target[1]);
+        for (int[] ghost : ghosts) {
+            int d = Math.abs(ghost[0] - target[0]) + Math.abs(ghost[1] - target[1]);
+            if (d <= max) return false;
+        }
+        return true;
+    }
+}
