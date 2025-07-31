312232777305c265de0d74eb21fe2259a577de42
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-07-31 12:19:13 +0530

    Create 1094-MatrixCellsInDistanceOrder

---
+++ 1094-MatrixCellsInDistanceOrder/1094-MatrixCellsInDistanceOrder.py
@@ -0,0 +1,9 @@
+# Last updated: 7/31/2025, 12:19:14 PM
+class Solution:
+    def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
+        def dist(point):
+            pi, pj = point
+            return abs(pi - r0) + abs(pj - c0)
+
+        points = [(i, j) for i in range(R) for j in range(C)]
+        return sorted(points, key=dist)
