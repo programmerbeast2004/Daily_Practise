6d6fe11c4ef989bd51d5c68109b4c4291e2abbae
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-07-31 12:17:10 +0530

    Create 3018-MakeStringASubsequenceUsingCyclicIncrements

---
+++ 3018-MakeStringASubsequenceUsingCyclicIncrements/3018-MakeStringASubsequenceUsingCyclicIncrements.java
@@ -0,0 +1,24 @@
+// Last updated: 7/31/2025, 12:17:11 PM
+class Solution {
+    public boolean canMakeSubsequence(String source, String target) {
+        int srcLen = source.length();
+        int tgtLen = target.length();
+        int srcIdx = 0;
+        int tgtIdx = 0;
+        
+        while (srcIdx < srcLen && tgtIdx < tgtLen) {
+            char srcChar = source.charAt(srcIdx);
+            char tgtChar = target.charAt(tgtIdx);
+            
+            if (srcChar == tgtChar || 
+                (srcChar == 'z' && tgtChar == 'a') || 
+                (srcChar + 1 == tgtChar)) {
+                srcIdx++;
+                tgtIdx++;
+            } else {
+                srcIdx++;
+            }
+        }
+        return tgtIdx == tgtLen;
+    }
+}
