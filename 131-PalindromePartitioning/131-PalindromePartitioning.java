255ecac6f86506477283b379579960139f7d50da
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-07-31 12:21:21 +0530

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -0,0 +1,36 @@
+// Last updated: 7/31/2025, 12:21:22 PM
+import java.util.*;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, List<String> currentPartition, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(currentPartition));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                currentPartition.add(s.substring(start, end + 1));
+                backtrack(s, end + 1, currentPartition, result);
+                currentPartition.remove(currentPartition.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
