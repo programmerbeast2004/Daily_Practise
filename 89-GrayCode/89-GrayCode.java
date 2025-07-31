3d756cf22c1fc74a6099b5ac7b9dea85b754f08a
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-07-31 12:21:38 +0530

    Create 89-GrayCode

---
+++ 89-GrayCode/89-GrayCode.java
@@ -0,0 +1,16 @@
+// Last updated: 7/31/2025, 12:21:39 PM
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+
+        for(int i = 1; i <= n; i++){
+            List<Integer> rev = new ArrayList<>(list);
+            Collections.reverse(rev);
+            int val = (int)Math.pow(2,i-1);
+            for(int v : rev) list.add(v+val);
+        }
+        
+        return list;
+    }
+}
