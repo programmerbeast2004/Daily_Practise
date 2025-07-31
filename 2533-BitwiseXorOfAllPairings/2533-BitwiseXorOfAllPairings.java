8bdcd63b8c31d1348f18c4f363837c5ad8b4fcb9
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-07-31 12:17:46 +0530

    Create 2533-BitwiseXorOfAllPairings

---
+++ 2533-BitwiseXorOfAllPairings/2533-BitwiseXorOfAllPairings.java
@@ -0,0 +1,14 @@
+// Last updated: 7/31/2025, 12:17:47 PM
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int answer = 0, n1= nums1.length, n2 = nums2.length;
+        if(n1%2 != 0)
+            for(int i =0; i < n2; i++)
+                answer ^= nums2[i];
+        if(n2%2 != 0)
+            for(int i =0; i < n1; i++)
+                answer ^= nums1[i];
+        return answer;
+        
+    }
+}
