391338f39cfe1d05a326c50bc57e340fd92fd544
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-08-14 11:12:03 +0530

    File updated at 8/14/2025, 11:12:03 AM

--- 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.java
+++ 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.java
@@ -1,4 +1,19 @@
-// Last updated: 8/14/2025, 11:10:29 AM
+// Last updated: 8/14/2025, 11:12:03 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 import java.util.*;
 
 class Solution {
@@ -6,29 +21,23 @@ class Solution {
         if (root == null) return 0;
 
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0));
         int width = 0;
+        q.add(new Pair(root, 0));
 
         while (!q.isEmpty()) {
-            int size = q.size();
-            int minIndex = q.peek().idx; // normalize to prevent overflow
-            int first = 0, last = 0;
-
-            for (int i = 0; i < size; i++) {
-                Pair current = q.poll();
-                int currIndex = current.idx - minIndex; // normalized index
-
-                if (i == 0) first = currIndex;
-                if (i == size - 1) last = currIndex;
-
-                if (current.node.left != null) {
-                    q.add(new Pair(current.node.left, 2 * currIndex + 1));
+            Pair start = q.peek();
+            int s = q.size();
+            Pair end = null; // Fixed: capital P
+            for (int i = 0; i < s; i++) {
+                end = q.poll(); // Fixed: add ()
+                if (end.node.left != null) {
+                    q.add(new Pair(end.node.left, 2 * end.idx + 1));
                 }
-                if (current.node.right != null) {
-                    q.add(new Pair(current.node.right, 2 * currIndex + 2));
+                if (end.node.right != null) {
+                    q.add(new Pair(end.node.right, 2 * end.idx + 2));
                 }
             }
-            width = Math.max(width, last - first + 1);
+            width = Math.max(width, end.idx - start.idx + 1);
         }
         return width;
     }
@@ -37,7 +46,7 @@ class Solution {
 class Pair {
     TreeNode node;
     int idx;
-    Pair(TreeNode node, int idx) {
+    Pair(TreeNode node, int idx) { // Fixed: constructor
         this.node = node;
         this.idx = idx;
     }
