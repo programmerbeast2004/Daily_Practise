d7ccbb851de07871effa940f1209e04a15ae671c
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-09-12 13:22:28 +0530

    Create 139-WordBreak

---
+++ 139-WordBreak/139-WordBreak.java
@@ -0,0 +1,18 @@
+// Last updated: 9/12/2025, 1:22:27 PM
+class Solution {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
