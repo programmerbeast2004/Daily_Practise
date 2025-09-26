4ef61fc8da6f4294f1694d5244fd093d0e2e6835
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-09-26 15:44:43 +0530

    File updated at 9/26/2025, 3:44:43 PM

--- 611-ValidTriangleNumber/611-ValidTriangleNumber.java
+++ 611-ValidTriangleNumber/611-ValidTriangleNumber.java
@@ -1,22 +1,17 @@
-// Last updated: 7/31/2025, 12:19:53 PM
+// Last updated: 9/26/2025, 3:44:43 PM
 class Solution {
     public int triangleNumber(int[] nums) {
         int n = nums.length;
         int count = 0;
-
-        // Sort the array so we can safely apply triangle condition
         Arrays.sort(nums);
-
-        // Try every triplet combination
         for (int i = 0; i < n - 2; i++) {
-            if (nums[i] == 0) continue; // A side with length 0 can't form a triangle
+            if (nums[i] == 0) continue;
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    // Triangle condition: a + b > c
                     if (nums[i] + nums[j] > nums[k]) {
                         count++;
                     } else {
-                        break; // Because array is sorted, no later k will work
+                        break;
                     }
                 }
             }
