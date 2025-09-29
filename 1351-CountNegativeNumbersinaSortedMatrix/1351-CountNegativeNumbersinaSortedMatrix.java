0d5a29a2bb05fba70552c42c3ba5ee0188b6d781
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-09-29 14:35:51 +0530

    File updated at 9/29/2025, 2:35:52 PM

--- 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.java
+++ 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.java
@@ -1,19 +1,17 @@
-// Last updated: 9/29/2025, 1:37:36 PM
+// Last updated: 9/29/2025, 2:35:52 PM
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int max=Integer.MIN_VALUE;
-        int m=accounts.length;
-        int n=accounts[0].length;
-        for(int i=0;i<m;i++){
-            int sum=0;
-            for(int j=0;j<n;j++){
-                sum+=accounts[i][j];
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int c=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int currsum=0;
+            for(int j=i;j<n;j++){
+                currsum+=nums[j];
+                if(currsum==goal){
+                    c++;
+                }
             }
-            if(max<=sum){
-                max=sum;
-            }
-
         }
-        return max;
+        return c;
     }
 }
