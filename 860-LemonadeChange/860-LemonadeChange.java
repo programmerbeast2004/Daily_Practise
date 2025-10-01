9b903c0e55bf2ecc89d42adf5f06af527549c9e9
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-10-01 15:37:51 +0530

    File updated at 10/1/2025, 3:37:50 PM

--- 860-LemonadeChange/860-LemonadeChange.java
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -1,15 +1,20 @@
-// Last updated: 10/1/2025, 3:18:03 PM
+// Last updated: 10/1/2025, 3:37:50 PM
 class Solution {
-    public void moveZeroes(int[] arr) {
-        int idx=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                arr[idx]=arr[i];
-                idx++;
-            }
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes,(a,b)-> Integer.compare(b[1],a[1]));
+        int ans=0;
+        for(int[] boxType: boxTypes){
+            int a=boxType[0];//No of Boxes
+            int b=boxType[1]; //No of Units
+
+            int Boxtotake=Math.min(truckSize,a); // Box that can be taken
+            ans+=b*Boxtotake; //Total Number Of Units
+            truckSize-=a; //Trcuk Size is decreasing
+        
+        if(truckSize<=0){
+            break; //Less than zero hote he rok do !!
         }
-        for(int i=idx;i<arr.length;i++){
-            arr[i]=0;
         }
+        return ans;
     }
 }
