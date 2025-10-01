277ea2fc7484e0d257ecb5664c16e00e4b09547f
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-10-01 15:18:04 +0530

    File updated at 10/1/2025, 3:18:03 PM

--- 860-LemonadeChange/860-LemonadeChange.java
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -1,33 +1,15 @@
-// Last updated: 10/1/2025, 2:19:56 PM
+// Last updated: 10/1/2025, 3:18:03 PM
 class Solution {
-    public boolean lemonadeChange(int[] arr) {
-        int five=0; int ten=0;
+    public void moveZeroes(int[] arr) {
+        int idx=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==5){
-                five++;
-            }
-            else if(arr[i]==10){
-                if(five>=1){
-                    five--;
-                    ten++;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-            if(five>=1 && ten >=1){
-                five--;
-                ten--;
-            }
-            else if(five>=3){
-                five=five-3;
-            }
-            else{
-                return false;
-            }
+            if(arr[i]!=0){
+                arr[idx]=arr[i];
+                idx++;
             }
         }
-        return true;
+        for(int i=idx;i<arr.length;i++){
+            arr[i]=0;
+        }
     }
 }
