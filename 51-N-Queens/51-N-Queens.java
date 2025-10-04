7819a1cbca0df4667d0ee5dcddcf4d539550c4fa
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-10-04 13:57:50 +0530

    File updated at 10/4/2025, 1:57:51 PM

--- 51-N-Queens/51-N-Queens.java
+++ 51-N-Queens/51-N-Queens.java
@@ -1,26 +1,12 @@
-// Last updated: 10/4/2025, 1:50:45 PM
+// Last updated: 10/4/2025, 1:57:51 PM
 class Solution {
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        helper(nums,0,new LinkedList<>(),ans);
-        return ans;
-    }
-    public static void helper(int[] nums,int s, LinkedList<Integer>path,List<List<Integer>> ans){
-        if(path.size()>1){
-            ans.add( new LinkedList<>(path));
-        }
-        Set<Integer>used=new HashSet<>();
-        for(int i=s;i<nums.length;i++){
-            if(used.contains(nums[i])){
-                continue;
-            }
-            if (path.isEmpty() || nums[i] >= path.getLast()) {
-                used.add(nums[i]);
-                path.addLast(nums[i]);
-                helper(nums,i+1,path,ans);
-                path.removeLast();
+    public int getMinDistance(int[] nums, int target, int start) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                min=Math.min(min, Math.abs(i-start));
             }
         }
-
+        return min;
     }
 }
