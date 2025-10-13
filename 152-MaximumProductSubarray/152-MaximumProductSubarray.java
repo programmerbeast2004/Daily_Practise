9be6de7f62cefd6fe2085400c554a9e3923a927b
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-10-13 19:39:27 +0530

    File updated at 10/13/2025, 7:39:29 PM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.java
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -1,18 +1,12 @@
-// Last updated: 10/13/2025, 7:17:24 PM
+// Last updated: 10/13/2025, 7:39:29 PM
 class Solution {
-    public int maxProduct(int[] nums) {
-        int pre=1; int suf=1;
-        int res=Integer.MIN_VALUE;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            pre*=nums[i];
-            res=Math.max(pre,res);
-            if(pre==0) pre=1;
-
-            suf*=nums[n-i-1];
-            res=Math.max(suf,res);
-            if(suf==0) suf=1;
+    public int maxAscendingSum(int[] arr) {
+        int curr=arr[0];
+        int ans=arr[0];
+        for(int i=1;i<arr.length;i++){
+            curr=arr[i]>arr[i-1] ? curr+arr[i]:arr[i];
+            ans=Math.max(curr,ans);
         }
-        return res;
+        return ans;
     }
 }
