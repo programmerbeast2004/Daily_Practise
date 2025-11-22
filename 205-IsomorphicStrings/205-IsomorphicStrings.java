8943cf9eab88edf8e0e2426502464dbd3efb1412
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-11-23 01:11:00 +0530

    File updated at 11/23/2025, 1:11:00 AM

--- 205-IsomorphicStrings/205-IsomorphicStrings.java
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -1,6 +1,23 @@
-// Last updated: 11/23/2025, 1:01:05 AM
+// Last updated: 11/23/2025, 1:11:00 AM
 class Solution {
-    public String toLowerCase(String s) {
-        return s.toLowerCase();
+    public String reverseOnlyLetters(String s) {
+        char[] arr=s.toCharArray();
+        int st=0, ed=arr.length-1;
+        while(st<ed){
+            if(Character.isLetter(arr[st]) && Character.isLetter(arr[ed]) ){
+                char temp=arr[st];
+                arr[st]=arr[ed];
+                arr[ed]=temp;
+                st++;
+                ed--;
+            }
+            else if(!Character.isLetter(arr[ed])){
+                ed--;
+            }
+            else{
+                st++;
+            }
+        }
+        return new String(arr);
     }
 }
