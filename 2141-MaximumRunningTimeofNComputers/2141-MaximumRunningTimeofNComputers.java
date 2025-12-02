874b8d312bebd1e1853b060162ca50262e6aeb86
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-12-02 22:09:09 +0530

    Create 2141-MaximumRunningTimeofNComputers

---
+++ 2141-MaximumRunningTimeofNComputers/2141-MaximumRunningTimeofNComputers.java
@@ -0,0 +1,20 @@
+// Last updated: 12/2/2025, 10:09:09 PM
+1class Solution {
+2    public long maxRunTime(int n, int[] batteries) {
+3        long te=0;
+4        for(int bt:batteries){
+5            te+=bt;
+6        }
+7        Arrays.sort(batteries);
+8        for(int i=batteries.length-1;i>=0;i--){
+9            if(batteries[i]>te/n){
+10                te-=batteries[i];
+11                n--;
+12            }
+13            else{
+14                break;
+15            }
+16        }
+17        return te/n;
+18    }
+19}
