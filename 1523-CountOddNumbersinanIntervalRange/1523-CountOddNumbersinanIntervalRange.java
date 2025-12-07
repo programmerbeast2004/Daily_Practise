708efff0f8729935150fe7c7e6560c14bb2c11a1
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2025-12-07 06:32:11 +0530

    File updated at 12/7/2025, 6:32:13 AM

--- 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.java
+++ 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.java
@@ -1,6 +1,13 @@
-// Last updated: 12/7/2025, 6:23:21 AM
+// Last updated: 12/7/2025, 6:32:13 AM
 1class Solution {
 2    public int countOdds(int low, int high) {
-3        return (high+1)/2-low/2;
-4    }
-5}
+3        if(low%2==0)low++;
+4        int c=0;
+5        for(int i=low;i<=high;i+=2){
+6            if(i%2!=0){
+7                c++;
+8            }
+9        }
+10        return c;
+11    }
+12}
