ebf2156f42b13768670658f18dd4f2ba35d070e5
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-01-01 11:42:17 +0530

    File updated at 1/1/2026, 11:42:17 AM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,17 +1,17 @@
-// Last updated: 7/31/2025, 12:21:57 PM
-class Solution {
-    public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
-        return digits;
-
-    }
-
-}
+// Last updated: 1/1/2026, 11:42:17 AM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        for (int i = digits.length - 1; i >= 0; i--) {
+4            if (digits[i] < 9) {
+5                digits[i]++;
+6                return digits;
+7            }
+8            digits[i] = 0;
+9        }
+10        digits = new int[digits.length + 1];
+11        digits[0] = 1;
+12        return digits;
+13
+14    }
+15
+16}
