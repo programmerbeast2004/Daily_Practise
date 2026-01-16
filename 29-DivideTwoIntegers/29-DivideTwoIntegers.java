e780e56c5c397e965a7806b4d2e4c876edcae743
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-01-16 08:49:06 +0530

    File updated at 1/16/2026, 8:49:06 AM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.java
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -1,39 +1,29 @@
-// Last updated: 7/31/2025, 12:22:27 PM
-class Solution {
-    public int divide(int dividend, int divisor) {
-        // Handle overflow case
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
-        }
-
-        // Get absolute values and handle edge case for Integer.MIN_VALUE
-        long absDividend = Math.abs((long) dividend);
-        long absDivisor = Math.abs((long) divisor);
-
-        // Initialize result
-        int result = 0;
-
-        // Perform division using bit shifting
-        while (absDividend >= absDivisor) {
-            long temp = absDivisor;
-            int multiple = 1;
-
-            // Find the largest multiple
-            while (absDividend >= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
-            }
-
-            // Subtract the largest multiple from the dividend
-            absDividend -= temp;
-            result += multiple;
-        }
-
-        // Determine the sign of the result
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
-            return -result;
-        } else {
-            return result;
-        }
-    }
-}
+// Last updated: 1/16/2026, 8:49:06 AM
+1class Solution {
+2    public int divide(int dvd, int dvr) {
+3        //1 
+4        if(dvd==Integer.MIN_VALUE && dvr==-1){
+5            return Integer.MAX_VALUE;
+6        }
+7        //2
+8        long a=Math.abs((long)dvd);
+9        long b=Math.abs((long)dvr);
+10
+11        //3
+12        int res=0;
+13        while(a>=b){
+14            long temp=b;
+15            int ct=1;
+16            while(a>=(temp<<1)){
+17                temp<<=1;
+18                ct<<=1;
+19            }
+20            a-=temp;
+21            res+=ct;
+22
+23        }
+24        //4
+25        if((dvd<0) ^ (dvr<0) )res=-res;
+26        return res;
+27    }
+28}
