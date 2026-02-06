6a438fdc22e4d67eb20ece6eb8bd13d87d0d74e4
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-02-06 14:29:56 +0530

    File updated at 2/6/2026, 2:29:57 PM

--- 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
+++ 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -1,18 +1,12 @@
-// Last updated: 2/6/2026, 2:07:36 PM
+// Last updated: 2/6/2026, 2:29:57 PM
 1class Solution {
 2    public int maxFrequency(int[] nums, int k) {
-3        int max=0,ck=0;
-4        int[] prefix=new int[51];
-5        for(int i=0;i<nums.length;i++){
-6            if(nums[i]==k){
-7                ck++;
-8                prefix[k]=max+1;
-9            }
-10            else{
-11                prefix[nums[i]]=Math.max(prefix[nums[i]],ck)+1;
-12            }
-13            max=Math.max(max,prefix[nums[i]]);
-14        }
-15        return max;
-16    }
-17}
+3        int count[]=new int[51];
+4        int res=0;
+5        for(int a:nums){
+6            count[a]=Math.max(count[a],count[k])+1;
+7            res=Math.max(res,count[a]-count[k]);
+8        }
+9        return count[k]+res;
+10    }
+11}
