e960d2ebf7c7fc44eda7b49df9fe6a9d276c88a2
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-02-24 16:14:09 +0530

    Create 3483-Unique3-DigitEvenNumbers

---
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.java
@@ -0,0 +1,36 @@
+// Last updated: 2/24/2026, 4:14:09 PM
+1class Solution {
+2    public int totalNumbers(int[] digits) {
+3        HashSet<Integer> set = new HashSet<>();
+4        int n = digits.length;
+5
+6        for (int i = 0; i < n; i++) {
+7            for (int j = 0; j < n; j++) {
+8                for (int k = 0; k < n; k++) {
+9                    if (i != j && j != k && i != k) {
+10                        int a = digits[i];
+11                        int b = digits[j];
+12                        int c = digits[k];
+13
+14                        // a != 0 ensures 3-digit number
+15                        // c % 2 == 0 ensures even number
+16                        if (a != 0 && c % 2 == 0) {
+17                            int res = a * 100 + b * 10 + c;
+18                            set.add(res);
+19                        }
+20                    }
+21                }
+22            }
+23        }
+24
+25        List<Integer> ans = new ArrayList<>(set);
+26        Collections.sort(ans);
+27
+28        int[] arr = new int[ans.size()];
+29        for (int i = 0; i < ans.size(); i++) {
+30            arr[i] = ans.get(i);
+31        }
+32
+33        return arr.length;
+34    }
+35}
