ffa4af9f03c4e709d26354dcefe404807126bd14
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:25:20 +0530

    Create 2747-ApplyTransformOverEachElementInArray

---
+++ 2747-ApplyTransformOverEachElementInArray/2747-ApplyTransformOverEachElementInArray.js
@@ -0,0 +1,12 @@
+// Last updated: 5/9/2026, 11:25:19 AM
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    for (let i = 0; i < arr.length; ++i) {
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
+};
