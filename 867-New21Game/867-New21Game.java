e7c373ab9ee5a8a8259bfe025df9dc757646a55a
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:30:14 +0530

    Create 867-New21Game

---
+++ 867-New21Game/867-New21Game.java
@@ -0,0 +1,21 @@
+// Last updated: 5/9/2026, 11:30:13 AM
+class Solution {
+    public double new21Game(int N, int K, int W) {
+        if (K == 0 || N >= K - 1 + W) return 1.0;
+        double[] dp = new double[N + 1];
+        dp[0] = 1.0;
+        double windowSum = 1.0, result = 0.0;
+        for (int i = 1; i <= N; i++) {
+            dp[i] = windowSum / W;
+            if (i < K) {
+                windowSum += dp[i];
+            } else {
+                result += dp[i];
+            }
+            if (i - W >= 0) {
+                windowSum -= dp[i - W];
+            }
+        }
+        return result;
+    }
+}
