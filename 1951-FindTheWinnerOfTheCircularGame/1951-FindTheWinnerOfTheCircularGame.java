03daa303c1dd79f983ded615a78c43f0b47a1a3c
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:27:20 +0530

    Create 1951-FindTheWinnerOfTheCircularGame

---
+++ 1951-FindTheWinnerOfTheCircularGame/1951-FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,10 @@
+// Last updated: 5/9/2026, 11:27:20 AM
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return solve(n,k)+1;
+    }
+    public int solve(int n,int k){
+        if(n==1) return 0;
+        return (solve(n-1,k)+k)%n;
+    }
+}
