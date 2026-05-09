1af9e517c0f615f38caf4a2a61a4cf88b635004c
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:30:45 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,21 @@
+// Last updated: 5/9/2026, 11:30:45 AM
+class Solution{
+    public int[] asteroidCollision(int[] asteroids){
+        Stack<Integer> st=new Stack<>();
+        for(int a:asteroids){
+            while(!st.isEmpty()&&st.peek()>0 && a<0){
+                int diff=st.peek()+a;
+                if(diff<0) st.pop();
+                else if(diff>0) a=0;
+                else {
+                    st.pop();
+                    a=0;
+                }
+            }
+            if(a!=0) st.push(a);
+        }
+        int[] res=new int[st.size()];
+        for(int i=res.length-1;i>=0;i--) res[i]=st.pop();
+        return res;
+    }
+}
