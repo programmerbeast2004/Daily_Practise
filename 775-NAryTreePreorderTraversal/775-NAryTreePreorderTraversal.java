778f1a934af7f572d6f30f262bb98d2757c096f5
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:30:34 +0530

    Create 775-NAryTreePreorderTraversal

---
+++ 775-NAryTreePreorderTraversal/775-NAryTreePreorderTraversal.java
@@ -0,0 +1,35 @@
+// Last updated: 5/9/2026, 11:30:33 AM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res=new ArrayList<>();
+        dfs(root,res);
+        return res;
+    }
+    public static void dfs(Node root,List<Integer> res){
+        if(root==null) return;
+        res.add(root.val);
+        for(Node child:root.children){
+            dfs(child,res);
+        }
+        //res.add(root.val);
+    }
+}
