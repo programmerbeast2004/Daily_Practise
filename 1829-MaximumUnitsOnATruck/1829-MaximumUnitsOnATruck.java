32278d7283e365f25c0ef2bdd23a589374a961e8
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:27:41 +0530

    Create 1829-MaximumUnitsOnATruck

---
+++ 1829-MaximumUnitsOnATruck/1829-MaximumUnitsOnATruck.java
@@ -0,0 +1,20 @@
+// Last updated: 5/9/2026, 11:27:40 AM
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes,(a,b)-> Integer.compare(b[1],a[1]));
+        int ans=0;
+        for(int[] boxType: boxTypes){
+            int a=boxType[0];//No of Boxes
+            int b=boxType[1]; //No of Units
+
+            int Boxtotake=Math.min(truckSize,a); // Box that can be taken
+            ans+=b*Boxtotake; //Total Number Of Units
+            truckSize-=a; //Trcuk Size is decreasing
+        
+        if(truckSize<=0){
+            break; //Less than zero hote he rok do !!
+        }
+        }
+        return ans;
+    }
+}
