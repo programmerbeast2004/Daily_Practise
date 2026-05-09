03dfc8fa9971d365423d2e7526afc1a2580f3371
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:26:13 +0530

    Create 2386-MinMaxGame

---
+++ 2386-MinMaxGame/2386-MinMaxGame.java
@@ -0,0 +1,18 @@
+// Last updated: 5/9/2026, 11:26:12 AM
+class Solution {
+    public int minMaxGame(int[] nums) {
+        int n=nums.length;
+        while(n>1){
+            for(int i=0;i<n/2;i++){
+                if(i%2==0){
+                    nums[i]=Math.min(nums[2*i],nums[2*i+1]);
+                }
+                else{
+                    nums[i]=Math.max(nums[2*i],nums[2*i+1]);
+                }
+            }
+            n/=2;
+        }
+        return nums[0];
+    }
+}
