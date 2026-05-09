72d2fce42ffdb956ada4c279b14193ccc31c4b8c
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:30:35 +0530

    Create 774-MaximumDepthOfNAryTree

---
+++ 774-MaximumDepthOfNAryTree/774-MaximumDepthOfNAryTree.java
@@ -0,0 +1,30 @@
+// Last updated: 5/9/2026, 11:30:35 AM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if(root==null)return 0;
+        int max=0;
+        for(Node child:root.children){
+            max=Math.max(max,maxDepth(child));
+        }
+        return max+1;
+    }
+}
