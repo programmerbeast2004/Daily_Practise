ee0678c25bba75cb6587c5d49dcc9b911451c3fe
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:26:33 +0530

    Create 2283-SortEvenAndOddIndicesIndependently

---
+++ 2283-SortEvenAndOddIndicesIndependently/2283-SortEvenAndOddIndicesIndependently.java
@@ -0,0 +1,28 @@
+// Last updated: 5/9/2026, 11:26:33 AM
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        List<Integer> even=new ArrayList<>();
+        List<Integer> odd=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                even.add(nums[i]);
+            }
+            else{
+                odd.add(nums[i]);
+            }
+        }
+        Collections.sort(even);
+        Collections.sort(odd,Collections.reverseOrder());
+        int e=0, o=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                nums[i]=even.get(e++);
+            }
+            else{
+                nums[i]=odd.get(o++);
+            }
+        }
+        return nums;
+
+    }
+}
