581cb07f27db24ee8aa5268191d0d6adb2092441
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:30:29 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 5/9/2026, 11:30:29 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int l=0,r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+}
