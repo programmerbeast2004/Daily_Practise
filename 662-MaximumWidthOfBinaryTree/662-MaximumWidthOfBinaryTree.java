14edeff5c404b2ac5e559084293a1bcafce46cb0
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:30:59 +0530

    Create 662-MaximumWidthOfBinaryTree

---
+++ 662-MaximumWidthOfBinaryTree/662-MaximumWidthOfBinaryTree.java
@@ -0,0 +1,53 @@
+// Last updated: 5/9/2026, 11:30:59 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<Pair> q = new LinkedList<>();
+        int width = 0;
+        q.add(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair start = q.peek();
+            int s = q.size();
+            Pair end = null; // Fixed: capital P
+            for (int i = 0; i < s; i++) {
+                end = q.poll(); // Fixed: add ()
+                if (end.node.left != null) {
+                    q.add(new Pair(end.node.left, 2 * end.idx + 1));
+                }
+                if (end.node.right != null) {
+                    q.add(new Pair(end.node.right, 2 * end.idx + 2));
+                }
+            }
+            width = Math.max(width, end.idx - start.idx + 1);
+        }
+        return width;
+    }
+}
+
+class Pair {
+    TreeNode node;
+    int idx;
+    Pair(TreeNode node, int idx) { // Fixed: constructor
+        this.node = node;
+        this.idx = idx;
+    }
+}
