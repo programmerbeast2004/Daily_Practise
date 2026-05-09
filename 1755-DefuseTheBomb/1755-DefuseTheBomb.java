72a6cc4ca7ed38c26f826455e831314480509f82
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-05-09 11:27:47 +0530

    Create 1755-DefuseTheBomb

---
+++ 1755-DefuseTheBomb/1755-DefuseTheBomb.java
@@ -0,0 +1,27 @@
+// Last updated: 5/9/2026, 11:27:46 AM
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int[]arr=new int[n];
+        if(k>0){
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n];
+                }
+                arr[i] = sum;
+            }
+        }
+        else if(k<0){
+            int K=Math.abs(k);
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= K; j++) {
+                    sum += code[(i-j+n) % n];
+                }
+                arr[i] = sum;
+            }
+        }
+        return arr;
+    }
+}
