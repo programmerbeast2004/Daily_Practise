9dbdc3a8adaafd1e53d01ca99e2605d1f45e9851
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-09-12 02:05:15 +0530

    Create 1696-JumpGameVI

---
+++ 1696-JumpGameVI/1696-JumpGameVI.java
@@ -0,0 +1,21 @@
+// Last updated: 9/12/2026, 2:05:15 AM
+1class Solution {
+2    public boolean canReach(String s, int minJump, int maxJump) {
+3        int n=s.length();
+4        boolean[]dp=new boolean[n];
+5        dp[0]=true;
+6
+7        int reach=0;
+8
+9        for(int i=1;i<n;i++){
+10            if(i-minJump>=0 && dp[i-minJump]){
+11                reach++;
+12            }
+13            if(i-maxJump-1>=0 && dp[i-maxJump-1]){
+14                reach--;
+15            }
+16            dp[i]=(reach>0 && s.charAt(i)=='0');
+17        }
+18        return dp[n-1];
+19    }
+20}
