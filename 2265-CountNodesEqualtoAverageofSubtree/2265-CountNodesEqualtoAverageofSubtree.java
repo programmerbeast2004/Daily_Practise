b48a22002977b23796185262cc968f18a2fab57a
Apoorv Mehrotra <apoorvmehrotra739@gmail.com>
2026-09-11 10:16:09 +0530

    Create 2265-CountNodesEqualtoAverageofSubtree

---
+++ 2265-CountNodesEqualtoAverageofSubtree/2265-CountNodesEqualtoAverageofSubtree.java
@@ -0,0 +1,24 @@
+// Last updated: 9/11/2026, 10:16:09 AM
+1class Solution {
+2    int count = 0;
+3
+4    public int averageOfSubtree(TreeNode root) {
+5        trav(root);
+6        return count;
+7    }
+8
+9    private int[] trav(TreeNode root) {
+10        if (root == null) return new int[]{0, 0};
+11
+12        int[] left = trav(root.left);
+13        int[] right = trav(root.right);
+14
+15        int subtreeSum = left[0] + right[0] + root.val;
+16        int subtreeCount = left[1] + right[1] + 1;
+17
+18        if (subtreeSum / subtreeCount == root.val)
+19            count++;
+20
+21        return new int[]{subtreeSum, subtreeCount};
+22    }
+23}
